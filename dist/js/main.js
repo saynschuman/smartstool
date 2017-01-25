3fd0892ca6c22cbf21679126cf0f0d05663841b7
saynschuman <saynschuman@gmail.com>
2017-01-25 14:23:48 +0200

    menu added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,48 @@
 $(document).ready(function() {
 
-$('.footer-menu').masonry({
-  // options... 
-  itemSelector: 'li'
-});
+	$('.footer-menu').masonry({
+	  itemSelector: 'li'
+	});
 
-});
+	
+	$('.menuItem[name="smart-1"').hover(
+	  function() {
+	  	$('.subMenu[name="smart-1"]').css('opacity', '1');  
+	  }, function() {
+	    $('.subMenu[name="smart-1"]').css('opacity', '0');  
+	  }
+	);
 
+	$('.menuItem[name="smart-2"').hover(
+	  function() {
+	  	$('.subMenu[name="smart-2"]').css('opacity', '1');  
+	  }, function() {
+	    $('.subMenu[name="smart-2"]').css('opacity', '0');  
+	  }
+	);
+
+	$('.menuItem[name="smart-3"').hover(
+	  function() {
+	  	$('.subMenu[name="smart-3"]').css('opacity', '1');  
+	  }, function() {
+	    $('.subMenu[name="smart-3"]').css('opacity', '0');  
+	  }
+	);
+
+	$('.menuItem[name="smart-4"').hover(
+	  function() {
+	  	$('.subMenu[name="smart-4"]').css('opacity', '1');  
+	  }, function() {
+	    $('.subMenu[name="smart-4"]').css('opacity', '0');  
+	  }
+	);
+
+	$('.menuItem[name="smart-5"').hover(
+	  function() {
+	  	$('.subMenu[name="smart-5"]').css('opacity', '1');  
+	  }, function() {
+	    $('.subMenu[name="smart-5"]').css('opacity', '0');  
+	  }
+	);				
+
+});	
