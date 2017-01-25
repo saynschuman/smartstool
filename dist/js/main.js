18656e04bab7d9a598ae1997d2a9d5e36d2d3c9d
saynschuman <saynschuman@gmail.com>
2017-01-25 16:32:56 +0200

    menu hover fixed

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,42 +7,87 @@ $(document).ready(function() {
 	
 	$('.menuItem[name="smart-1"').hover(
 	  function() {
-	  	$('.subMenu[name="smart-1"]').css('opacity', '1');  
+	  	$('.subMenu[name="smart-1"]').css('display', 'block');  
 	  }, function() {
-	    $('.subMenu[name="smart-1"]').css('opacity', '0');  
+	    $('.subMenu[name="smart-1"]').css('display', 'none');  
 	  }
 	);
 
 	$('.menuItem[name="smart-2"').hover(
 	  function() {
-	  	$('.subMenu[name="smart-2"]').css('opacity', '1');  
+	  	$('.subMenu[name="smart-2"]').css('display', 'block');  
 	  }, function() {
-	    $('.subMenu[name="smart-2"]').css('opacity', '0');  
+	    $('.subMenu[name="smart-2"]').css('display', 'none');  
 	  }
 	);
 
 	$('.menuItem[name="smart-3"').hover(
 	  function() {
-	  	$('.subMenu[name="smart-3"]').css('opacity', '1');  
+	  	$('.subMenu[name="smart-3"]').css('display', 'block');  
 	  }, function() {
-	    $('.subMenu[name="smart-3"]').css('opacity', '0');  
+	    $('.subMenu[name="smart-3"]').css('display', 'none');  
 	  }
 	);
 
 	$('.menuItem[name="smart-4"').hover(
 	  function() {
-	  	$('.subMenu[name="smart-4"]').css('opacity', '1');  
+	  	$('.subMenu[name="smart-4"]').css('display', 'block');  
 	  }, function() {
-	    $('.subMenu[name="smart-4"]').css('opacity', '0');  
+	    $('.subMenu[name="smart-4"]').css('display', 'none');  
 	  }
 	);
 
 	$('.menuItem[name="smart-5"').hover(
 	  function() {
-	  	$('.subMenu[name="smart-5"]').css('opacity', '1');  
+	  	$('.subMenu[name="smart-5"]').css('display', 'block');  
 	  }, function() {
-	    $('.subMenu[name="smart-5"]').css('opacity', '0');  
+	    $('.subMenu[name="smart-5"]').css('display', 'none');  
 	  }
-	);				
+	);	
+	$('.subMenu[name="smart-1"]').hover(
+	  function() {
+	  	$(this).css('display', 'block');  
+	  	$('.menuItem[name="smart-1"').addClass('active');
+	  }, function() {
+	    $(this).css('display', 'none');  
+	    $('.menuItem[name="smart-1"').removeClass('active');
+	  }
+	);	
+	$('.subMenu[name="smart-2"]').hover(
+	  function() {
+	  	$(this).css('display', 'block');  
+	  	$('.menuItem[name="smart-2"').addClass('active');
+	  }, function() {
+	    $(this).css('display', 'none');
+	    $('.menuItem[name="smart-2"').removeClass('active');  
+	  }
+	);	
+	$('.subMenu[name="smart-3"]').hover(
+	  function() {
+	  	$(this).css('display', 'block');  
+	  	$('.menuItem[name="smart-3"').addClass('active');
+	  }, function() {
+	    $(this).css('display', 'none'); 
+	    $('.menuItem[name="smart-3"').removeClass('active'); 
+	  }
+	);	
+	$('.subMenu[name="smart-4"]').hover(
+	  function() {
+	  	$(this).css('display', 'block'); 
+	  	$('.menuItem[name="smart-4"').addClass('active'); 
+	  }, function() {
+	    $(this).css('display', 'none'); 
+	    $('.menuItem[name="smart-4"').removeClass('active'); 
+	  }
+	);	
+	$('.subMenu[name="smart-5"]').hover(
+	  function() {
+	  	$(this).css('display', 'block'); 
+	  	$('.menuItem[name="smart-5"').addClass('active');
+	  }, function() {
+	    $(this).css('display', 'none'); 
+	    $('.menuItem[name="smart-5"').removeClass('active'); 
+	  }
+	);	
 
 });	
