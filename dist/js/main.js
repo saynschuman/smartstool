0f9bcaa540c0abcce8005176edc324569bffe6e2
saynschuman <saynschuman@gmail.com>
2017-01-30 13:24:38 +0200

    just video left

--- dist/js/main.js
+++ dist/js/main.js
@@ -108,5 +108,31 @@ $(document).ready(function() {
 		$(this).toggleClass('orange-border');
 	})
 
+	// swiper 
+
+  $('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:0,
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:1,
+	            nav:true
+	        },
+	        768:{
+	            items:2,
+	            nav:false
+	        },
+	        1200:{
+	            items:3,
+	            nav:true,
+	            loop:false
+	        }
+	    }
+	})
+
+  // scroll 
+
+  smoothScroll.init();
 	
 });	
