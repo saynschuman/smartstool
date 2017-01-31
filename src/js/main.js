f2fc48932cd242f098479729ce4fd2f1e3186f2f
saynschuman <saynschuman@gmail.com>
2017-01-31 12:06:51 +0200

    homepage finished

--- src/js/main.js
+++ src/js/main.js
@@ -172,4 +172,38 @@ $(document).ready(function() {
     })
 	
 	wow_2.init();
+
+
+	// wripple 
+
+
+	var ink, d, x, y;
+	$(".ripplelink").mouseenter(function(e){
+    if($(this).find(".ink").length === 0){
+        $(this).prepend("<span class='ink'></span>");
+    }
+         
+    ink = $(this).find(".ink");
+    ink.removeClass("animate");
+     
+    if(!ink.height() && !ink.width()){
+        d = Math.max($(this).outerWidth(), $(this).outerHeight());
+        ink.css({height: d, width: d});
+    }
+     
+    x = e.pageX - $(this).offset().left - ink.width()/2;
+    y = e.pageY - $(this).offset().top - ink.height()/2;
+     
+    ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+
+	});
+	
+$( 'main.homepage section.for-all .under' ).hover(
+  function() {
+    $( this ).addClass( "opacity" );
+  }, function() {
+    $( this ).removeClass( "opacity" );
+  }
+);	
+
 });	
